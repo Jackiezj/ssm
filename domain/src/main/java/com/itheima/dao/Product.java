ef9047816b87192b7aecce3a02c50f4b0b78a34c
jackiezz <jackiezz@163.com>
2019-08-28 22:11:46 +0800

    商品查询操作

--- domain/src/main/java/com/itheima/dao/Product.java
+++ domain/src/main/java/com/itheima/dao/Product.java
@@ -1,5 +1,8 @@
 package com.itheima.dao;
 
+import com.itheima.utils.DateUtils;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.util.Date;
 
 public class Product {
@@ -7,6 +10,7 @@ public class Product {
     private String productNum; // 编号 唯一
     private String productName; // 名称
     private String cityName; // 出发城市
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     private Date departureTime; // 出发时间
     private String departureTimeStr;
     private double productPrice; // 产品价格
@@ -55,6 +59,9 @@ public class Product {
     }
 
     public String getDepartureTimeStr() {
+        if (departureTime != null) {
+            departureTimeStr = DateUtils.date2String(departureTime, "yyyy-MM-dd HH:mm:ss");
+        }
         return departureTimeStr;
     }
 
@@ -87,6 +94,12 @@ public class Product {
     }
 
     public String getProductStatusStr() {
+        if (productStatus == 1) {
+            productStatusStr = "开启";
+        }
+        if (productStatus == 0) {
+            productStatusStr = "关闭";
+        }
         return productStatusStr;
     }
 
